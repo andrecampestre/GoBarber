2f822bb2fe308ae1fdf8b32fcf0f8c43989d4f11
Andre Loiola <andrecampestre@hotmail.com>
2019-07-14 16:52:50 -0300

    Atualização de código.

--- src/config/auth.js
+++
@@ -1,8 +0,0 @@
-export default {
-<<<<<<< HEAD
-  secret: 'f29618255c309de4469993cce24286ea',
-=======
-  secret: 'f29618255c309de4469993cce24286ea';
->>>>>>> 31f439a355e03cfad945dfd5ec1af6e84f1e2dcf
-  expiresIn: '7d',
-};
